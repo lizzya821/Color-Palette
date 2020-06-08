dadbf595e1e3580193b1a535e058aaac2eba3d7d
lizzya821 <lizzya821@gmail.com>
2020-06-08 14:43:09 -0500

    few last minute changes

---
+++ build/precache-manifest.31623ab3bac91076f287f6939c202530.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d16edb91366c205d891430c0c3ca554",
+    "url": "/Color-Palette/index.html"
+  },
+  {
+    "revision": "14c5fa97b7d8807730f2",
+    "url": "/Color-Palette/static/css/main.acd46897.chunk.css"
+  },
+  {
+    "revision": "bca5600125a69156531a",
+    "url": "/Color-Palette/static/js/2.aa838ef3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Color-Palette/static/js/2.aa838ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c5fa97b7d8807730f2",
+    "url": "/Color-Palette/static/js/main.c99757e1.chunk.js"
+  },
+  {
+    "revision": "d0a48d66502e48d239b1",
+    "url": "/Color-Palette/static/js/runtime-main.ead1a1b3.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/Color-Palette/static/media/mintbean.cf18b629.png"
+  }
+]);
